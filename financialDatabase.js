fc7ce54170fa5976b9128d3b4ba9a7db23404918
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-16 16:03:20 -0500

    added initial database, need to finish

---
+++ financialDatabase.js
@@ -0,0 +1,23 @@
+const financialDatabase = {}
+
+financialDatabase.bankAccounts = []
+financialDatabase.businesses = []
+financialDatabase.blackMailed = []
+financialDatabase.laundered = []
+
+const bankAccounts = {
+    name: "Bank Accounts", 
+    listOf: ["Bank of United Kingdom - 123456789011", "HMS Bank of Kenya - 34567890123", "Bank of Uzbekistan - 89012345678"]
+}
+const businesses = {
+    name: "Businesses",
+    listOf: ["Agro-King", "Sleuths-R-Us", "The Detectives Initiative"]
+}
+const blackMailed = {
+    name: "Blackmailed Officials",
+    listOf: ["Queen Elizabeth II", "Nelson Mandela", "Mayor Meghan Barry" ]
+}
+const laundered = {
+    name: "Money Laundering Businesses",
+    listOf: ["Fabulous Nails Inc", "Wash-o-matic Laundry"]
+}
